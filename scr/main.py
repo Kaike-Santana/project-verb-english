cd9d54736b2698f57f52b106f749308d9ff186b4
Kaike Santana <kaike1010@hotmail.com.br>
2024-04-17 15:42:48 -0300

    dividindo em 2 pasta, scr de código e verb de classes

--- scr/main.py
+++ scr/main.py
@@ -1,10 +1,11 @@
+import sys
+sys.path.append('C:/Users/altnet/Documents/JornadaDados/ProjectVerbEnglish/verb/')
 from verb import Verb
 
 def main():
     verbo = input(f"Digite um verbo em inglês: ")
 
     verbo_obj = Verb(verbo)
-    
     print("No passado (Simple Past):", verbo_obj.passado())
     print("No presente (Simple Present):", verbo_obj.presente())
     print("No futuro (Simple Future):", verbo_obj.futuro())
